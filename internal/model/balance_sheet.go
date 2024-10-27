4380eff1a9fa5101fea483532a3a64933397a558
guiparpineli <guiparpineli@hotmail.com>
2024-10-27 14:40:50 -0300

    FEAT: adicionado repository e com db

--- internal/model/balance_sheet.go
+++ internal/model/balance_sheet.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type BalanceSheet struct {
 	gorm.Model
-	Month        uint16        `gorm:"type:int;NOT NULL" json:"month"`
-	Year         uint16        `gorm:"type:int;NOT NULL" json:"year"`
+	Month        int           `gorm:"type:int;NOT NULL" json:"month"`
+	Year         int           `gorm:"type:int;NOT NULL" json:"year"`
 	Status       BalanceStatus `gorm:"type:varchar(12);NOT NULL" json:"status"`
 	Company      *Company      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"company"`
 	Account      *Account      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"account"`
--- internal/repository/balance_repository.go
+++ internal/repository/balance_repository.go
@@ -1,11 +1,46 @@
 package repository
 
 import (
-	"clear-api/internal/config"
+	"clear-api/internal/data"
 	"clear-api/internal/model"
+	"errors"
 	"gorm.io/gorm"
+	"time"
 )
 
+func checkBalance(balance *model.BalanceSheet) error {
+	if balance.Year != time.Now().Year() {
+		balance.Year = time.Now().Year()
+	}
+	if balance.Month < 1 || balance.Month > 12 {
+		return errors.New("month is invalid")
+	}
+	balance.Status = 0
+	return nil
+}
+
 func SaveBalance(balance *model.BalanceSheet) (*gorm.DB, error) {
-	return config.DB.Create(&balance), nil
+	err := checkBalance(balance)
+	if err != nil {
+		return nil, err
+	}
+	return data.DB.Create(&balance), nil
+}
+
+func UpdateBalance(balance *model.BalanceSheet) (*gorm.DB, error) {
+	var balanceSaved model.BalanceSheet
+	if err := data.DB.Where("id = ?", &balance.ID).First(&balanceSaved).Error; err != nil {
+		return nil, err
+	}
+
+	newBalance := model.BalanceSheet{
+		Month:        balance.Month,
+		Year:         balanceSaved.Year,
+		Status:       1,
+		Company:      balanceSaved.Company,
+		Account:      balanceSaved.Account,
+		Compositions: append(balanceSaved.Compositions, balance.Compositions...),
+	}
+
+	return data.DB.Save(&newBalance), nil
 }
